e2ac54b7548c5c4bd37053bd3de5fc4832b61078
Paris Ward <wardparis.pw@gmail.com>
2018-07-08 00:17:34 -0700

    Deleted angular-element nav, added bootstrap nav with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { WorkComponent } from './components/work/work.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent },
   {path: 'work', component: WorkComponent },
   {path: 'contact', component: ContactComponent }
