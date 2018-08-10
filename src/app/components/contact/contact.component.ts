2b54a2fc0cb31e61d9f33aa5ce7090584fe9b3c9
Paris Ward <wardparis.pw@gmail.com>
2018-08-09 17:02:28 -0700

    contact form validation work

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Email } from '../../models/Email';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
