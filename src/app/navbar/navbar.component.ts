8af7fc60f42c136b6872e9628f4d1afdc33ce14c
Paris Ward <wardparis.pw@gmail.com>
2019-02-27 05:50:09 -0800

    minor fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -16,10 +16,6 @@ export class NavbarComponent implements OnInit {
     window.open("https://github.com/ParisBueller")
   }
 
-  openTwitter() {
-    window.open("https://twitter.com/messages/compose?recipient_id=762079861766705152")
-  }
-
   openLinkedIn() {
     window.open("https://www.linkedin.com/in/paris-ward-97210a16b/")
   }
